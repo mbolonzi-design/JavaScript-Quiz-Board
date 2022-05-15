a6bda8d10d12ef54902669ea01f19198e5761817
mbolonzi-design <mbolonzi.design@gmail.com>
2022-05-15 15:07:52 +0300

    initialize constants

---
+++ assets/pages/js/app.js
@@ -0,0 +1,15 @@
+//selecting all required elements
+const start_btn = document.querySelector(".start-btn button");
+const container = document.querySelector(".container");
+const quit_btn = container.querySelector(".buttons .quit");
+const next_btn = container.querySelector(".buttons .next-btn");
+const quiz_box = document.querySelector(".quiz-box");
+const results_box = document.querySelector(".results-box");
+const option_list = document.querySelector("option-list");
+const time_line = document.querySelector("header .time-line");
+const timeText = document.querySelector(".timer .time-left-txt");
+const timeCount = document.querySelector(".timer .timer-sec");
+
+// start_btn.onclick = () => {
+//     container.classList.add("activeInfo");
+// }
