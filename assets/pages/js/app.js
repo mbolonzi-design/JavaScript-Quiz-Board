fe40a29c470de7b1971c07f6a8c25e07f28b5418
mbolonzi-design <mbolonzi.design@gmail.com>
2022-05-15 15:20:56 +0300

    show container box on clicking button

--- assets/pages/js/app.js
+++ assets/pages/js/app.js
@@ -1,15 +1,15 @@
 //selecting all required elements
-const start_btn = document.querySelector(".start-btn button");
+const start_btn = document.querySelector(".start_btn button");
 const container = document.querySelector(".container");
 const quit_btn = container.querySelector(".buttons .quit");
-const next_btn = container.querySelector(".buttons .next-btn");
-const quiz_box = document.querySelector(".quiz-box");
-const results_box = document.querySelector(".results-box");
-const option_list = document.querySelector("option-list");
-const time_line = document.querySelector("header .time-line");
-const timeText = document.querySelector(".timer .time-left-txt");
-const timeCount = document.querySelector(".timer .timer-sec");
+const next_btn = container.querySelector(".buttons .next_btn");
+const quiz_box = document.querySelector(".quiz_box");
+const results_box = document.querySelector(".results_box");
+const option_list = document.querySelector("option_list");
+const time_line = document.querySelector("header .time_line");
+const timeText = document.querySelector(".timer .time_left_txt");
+const timeCount = document.querySelector(".timer .timer_sec");
 
-// start_btn.onclick = () => {
-//     container.classList.add("activeInfo");
-// }
+start_btn.onclick = () => {
+    container.classList.add("activeInfo");
+}
