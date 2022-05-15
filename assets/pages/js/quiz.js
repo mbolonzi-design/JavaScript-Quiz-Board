33bfc9d67950f349cb66f61c942accf01b756cf1
mbolonzi-design <mbolonzi.design@gmail.com>
2022-05-15 20:04:42 +0300

    create an array of questions

---
+++ assets/pages/js/quiz.js
@@ -0,0 +1,33 @@
+// creating an array and passing the question number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: 'The "function" and "var" are known as:',
+    answer: 'Declaration statements',
+    options: ['Keywords','Prototypes','Data types','Declaration statements']
+  },
+    {
+    numb: 2,
+    question: 'Which JavaScript method is used to write HTML output?',
+    answer: 'document.write()',
+    options:['document.output()','document.writeHTML()','console.log()','document.write()']
+  },
+    {
+    numb: 3,
+    question: 'Which JavaScript method is used to access an HTML element by id?',
+    answer: 'getElementById(id)',
+    options: ['elementById(id)','getElement(id)','getElementById(id)','getElementById()']
+  },
+    {
+    numb: 4,
+    question: 'Which symbol is used separate JavaScript statements?',
+    answer: 'Semicolon(;)',
+    options: ['Comma (,)','Semicolon (;)','Hyphen (_)','Colon (:)']
+  },
+    {
+    numb: 5,
+    question: 'Which type of language is JavaScript?',
+    answer:'Object-Based' ,
+    options: [ 'Object-Based','Object-Oriented','Assembly-language','High-level']
+  }
+]
