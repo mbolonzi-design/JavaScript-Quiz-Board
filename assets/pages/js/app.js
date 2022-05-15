8e4de6e9063fcc4821b0f5e9a5fcaa8cd672dac4
mbolonzi-design <mbolonzi.design@gmail.com>
2022-05-15 16:16:37 +0300

    hide container box when exit button is clicked

--- assets/pages/js/app.js
+++ assets/pages/js/app.js
@@ -9,7 +9,12 @@ const option_list = document.querySelector("option_list");
 const time_line = document.querySelector("header .time_line");
 const timeText = document.querySelector(".timer .time_left_txt");
 const timeCount = document.querySelector(".timer .timer_sec");
-
+//start button
 start_btn.onclick = () => {
     container.classList.add("activeInfo");
+}
+
+//exit button
+quit_btn.onclick = () => {
+    container.classList.remove("activeInfo");
 }
