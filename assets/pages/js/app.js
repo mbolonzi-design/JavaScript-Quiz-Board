d283b2e084e7ddce2956470af71a3f405d0fca89
mbolonzi-design <mbolonzi.design@gmail.com>
2022-05-15 18:55:09 +0300

    gets test questions from the array

--- assets/pages/js/app.js
+++ assets/pages/js/app.js
@@ -87,3 +87,23 @@ next_btn.onclick = ()=>{
         showResults();//calling showResult function
     }
 }
+// getting questions and answers from the array
+function showQuetions(index){
+    const question_text = document.querySelector(".question_text");
+
+    //creating a new span and div tag for question and option and passing the value using array index
+    let question_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
+    let option_tag = '<div class="option"><span>'+ questions[index].options[0] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[1] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[2] +'</span></div>'
+    + '<div class="option"><span>'+ questions[index].options[3] +'</span></div>';
+    question_text.innerHTML = question_tag; //adding new span tag inside que_tag
+    option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+    
+    const option = option_list.querySelectorAll(".option");
+
+    // set onclick attribute to all available options
+    for(i=0; i < option.length; i++){
+        option[i].setAttribute("onclick", "optionSelected(this)");
+    }
+}
